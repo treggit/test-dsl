284a9d58cab75ba1a30f7b68af6ce44dea54a17e
anton.shelepau <anton.shelepov@jetbrains.com>
2025-06-16 10:12:23 +0200

    Test dsl

--- .teamcity/_Self/CloudInfrastructure.kt
+++ .teamcity/_Self/CloudInfrastructure.kt
@@ -6,14 +6,12 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.projectFeatures.s3Storage
 
 object CloudInfrastructure : Project({
 
-    name = "Cloud Infrastructure"
-    id = RelativeId("CloudInfrastructure")
-
     features {
         s3Storage {
             id = "tc-cloud-PROJECT_EXT_1"
 
-            bucketPrefix = "superb-leakey-fq7j9p"
+            bucketPrefix = "artifacts/superb-leakey-fq7j9p"
+            bucketName = "common-teamcityhosteddev"
         }
     }
 })
